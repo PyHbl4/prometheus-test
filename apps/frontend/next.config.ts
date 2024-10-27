5685f6ff9f77ba5ad7a4ff23ccf9f9ec99a49e78
Maxim <runkov@jetstyle.ru>
2024-10-27 18:40:43 +0500

    some fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  apiUrl: process.env.API_URL || 'http://localhost:3000/products',
+  apiUrl: process.env.API_URL || 'http://localhost:3200/products',
 };
 
 export default nextConfig;
